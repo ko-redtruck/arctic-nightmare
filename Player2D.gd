c915268019cdd83e8b5d9ff1881ecda6fa0c37c5
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-12 15:22:52 +0100

    Add Rigidbody Items to pick_up/drop/use

--- Player2D.gd
+++ Player2D.gd
@@ -11,7 +11,52 @@ var velocity = Vector2()
 func _ready():
 	pass # Replace with function body.
 
+func drop_equipped_item():
+	var current_item = get_equipped_item()
+	$ItemHolder.remove_child(current_item)
+	current_item.get_node("CollisionShape2D").disabled = false
+	current_item.set_mode(RigidBody2D.MODE_RIGID)
+	current_item.position = self.position + $ItemHolder.position
+	self.get_parent().add_child(current_item)
 
+func has_equipped_item():
+	return self.get_equipped_item() != null
+
+func use_equipped_item():
+	return self.get_equipped_item().use_on(self)
+	
+func equip(item):
+	if self.has_equipped_item():
+		self.drop_equipped_item()
+		
+	print("Equipping ",item, item.get_node("CollisionShape2D"))
+	item.get_node("CollisionShape2D").disabled = true
+	item.set_mode(RigidBody2D.MODE_STATIC)
+	
+	item.position = Vector2(0, 0)
+	$ItemHolder.add_child(item)
+ 
+func kill():
+	print("Player died")
+	
+func get_equipped_item():
+	if $ItemHolder.get_child_count() < 1:
+		return null
+	else:
+		return $ItemHolder.get_child(0)
+	
+func get_nearest_item_in_world():
+	var distance_to_nearest_item = INF
+	var nearest_item = null
+	var current_item = self.get_equipped_item()
+	for node in $PickUpArea.get_overlapping_bodies():
+			if node.is_in_group("Items") and node != current_item:
+				var distance_to_item = node.position.distance_to(self.position)
+				if distance_to_item < distance_to_nearest_item:
+					nearest_item = node
+					distance_to_nearest_item = distance_to_item
+	return nearest_item
+				
 func _physics_process(delta):
 	var walk_force = WALK_SPEED * (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
 	if abs(walk_force) < 0.2 * WALK_SPEED:
@@ -29,8 +74,14 @@ func _physics_process(delta):
 	if (is_on_floor() or is_on_wall()) and Input.is_action_just_pressed("ui_up"):
 		velocity.y += -JUMP_SPEED
 		
-	if Input.is_action_just_pressed("ui_pickup"):
-		for node in $PickupArea.get_overlapping_bodies():
-			if node.is_in_group("Items"):
-				print(node)
-
+	if Input.is_action_just_pressed("ui_pick_up_or_drop"):
+		var item = get_nearest_item_in_world()
+		if item != null:
+			item.pick_up_on(self)
+		elif self.has_equipped_item():
+			self.drop_equipped_item()
+	
+	if Input.is_action_just_pressed("ui_use"):
+		if self.has_equipped_item():
+			self.use_equipped_item()
+			
---
+++ items/Item.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func pick_up_on(player):
+	print("Picked up by", player)
+	self.get_parent().remove_child(self)
+	player.equip(self)
+	
+	
+func use_on(player):
+	print("Used on", player)
+	
+	
+func is_usable():
+	print("is Usable")
+	return true
