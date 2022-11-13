3d65d52de93f8e2f8ca0f6377d871525f1d72b3d
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-13 01:57:45 +0100

    Add PropellerHat

--- items/PropellerHat.gd
+++ items/PropellerHat.gd
@@ -1,18 +1,19 @@
 extends "res://items/Item.gd"
 
-export (bool) var can_fly = true
+export (bool) var CAN_FLY = true
 
 
 export (float) var FLY_FORCE = 20.0
 var cooldown_over = true
 
 func use_on(player):
-	if cooldown_over:
+	if self.is_usable():
 		player.velocity.y -= FLY_FORCE
 		$CooldownTimer.start()
 		cooldown_over = false
 
-
+func is_usable():
+	return cooldown_over and self.CAN_FLY
 func _on_CooldownTimer_timeout():
 	cooldown_over = true
 	
