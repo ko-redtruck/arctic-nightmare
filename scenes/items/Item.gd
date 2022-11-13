47f5bb606715c1dce32e7853885da31e47827494
Marius Dörbandt <github.marius@doerbandt.de>
2022-11-13 13:41:39 +0100

    stuuuuff

--- scenes/items/Item.gd
+++ scenes/items/Item.gd
@@ -1,23 +1,19 @@
-extends Node
+extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func is_in_effect_range(group: String):
+	for node in $EffectRadius.get_overlapping_bodies():
+		if node.is_in_group(group):
+			return true
+	return false
 
 func pick_up_on(player):
 	print("Picked up by", player)
 	self.get_parent().remove_child(self)
 	player.equip(self)
-	
+
 func use_on(player):
 	print("Used on", player)
-	
+
 func is_usable():
 	# print("is Usable")
 	return true
