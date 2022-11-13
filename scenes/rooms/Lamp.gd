0f6e1d800ca98248a998c4824d60150e229cfe81
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-13 01:22:14 +0100

    Add rotating lamp scene

---
+++ scenes/rooms/Lamp.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+export (float) var MAX_ROTATION
+export (float) var ROTATION_SPEED
+
+var phase = 0
+var rotation_direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	self.phase = rand_range(0, 2 * PI)
+	print("Phase:",phase)
+
+
+
+func _process(delta):
+	phase += delta * ROTATION_SPEED
+	self.rotation = sin(phase) * MAX_ROTATION
