2594f3208f5bb64eb3d9e935ec4d33a6a4f0dda4
Pungitius <>
2022-11-13 03:12:21 +0100

    fixed item gravity

--- scenes/dying_transition.gd
+++ scenes/dying_transition.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 	if GameState.isSleeping:
-		$Label.text = "You activate the swapping device while still sleeping and die in the real world..."
+		$Label.text = "You activate the swapping device in your dreams and don't wake up in time..."
 	else:
 		$Label.text = "You just killed yourself in the real world..."
 
